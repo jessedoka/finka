cd123022a0031efea31888d44f7e68c3e3999f4c
jessedoka <jdoka42@gmail.com>
2024-06-23 22:56:30 +0100

    chore: update tailwind.config.ts with new color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      "primary": "#1C1C1C",
+      "secondary": "#DADDD8",
+      "accent": "#ECEBE4",
+      "error": "#B00020",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
