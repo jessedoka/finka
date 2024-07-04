fd5f377092586e80a646c69ce78ec67b0530ec9c
jessedoka <jdoka42@gmail.com>
2024-07-04 01:58:41 +0100

    refactor: Move submit-button component to components directory and update imports
    changed handlng user creation

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -13,7 +13,7 @@ export function SubmitButton({ children, pendingText, ...props }: Props) {
   const isPending = pending && action === props.formAction;
 
   return (
-    <button {...props} type="submit" aria-disabled={pending}>
+    <button {...props} type="submit" aria-disabled={pending} className="w-full text-white bg-orange-400 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 transition duration-300 ease-in-out">
       {isPending ? pendingText : children}
     </button>
   );
